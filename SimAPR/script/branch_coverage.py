7eb1dc049deadcb06c101a7c07d1eb56145cbada
FreddyYJ <kyj1411@unist.ac.kr>
2023-03-30 03:03:50 +0000

    Remove plots in branch coverage

--- SimAPR/script/branch_coverage.py
+++ SimAPR/script/branch_coverage.py
@@ -1,10 +1,3 @@
-"""
-1. 각 패치별로 branch coverage 계산해서 vector로 파싱
-2. 벡터의 N1 norm과 N2 norm 계산
-3. pairwise comparison 수행
-4. 각 비교 결과를 이용해 plot으로: valid/invalid 패치를 따로 해서 locality 있는지 표시
-"""
-
 import json
 import os
 import sys
@@ -92,59 +85,6 @@ def compute_norms(output_file:str,branch_dir:str) -> Tuple[Dict[str,Dict[float,i
 
     return valid_l1_norms,valid_l2_norms,invalid_l1_norms,invalid_l2_norms
 
-def plot(output_dir:str,valid_l1_norms:Dict[str,Dict[float,int]],valid_l2_norms:Dict[str,Dict[float,int]],invalid_l1_norms:Dict[str,Dict[float,int]],invalid_l2_norms:Dict[str,Dict[float,int]]):
-    for test_name in invalid_l1_norms.keys():
-        plt.clf()
-        if test_name in valid_l1_norms.keys():
-            valid_l1=valid_l1_norms[test_name]
-            valid_l2=valid_l2_norms[test_name]
-        else:
-            valid_l1=dict()
-            valid_l2=dict()
-        invalid_l1=invalid_l1_norms[test_name]
-        invalid_l2=invalid_l2_norms[test_name]
-
-        valid_l1_x=[]
-        valid_l1_y=[]
-        valid_l2_x=[]
-        valid_l2_y=[]
-        invalid_l1_x=[]
-        invalid_l1_y=[]
-        invalid_l2_x=[]
-        invalid_l2_y=[]
-
-        valid_l1_keys=sorted(list(valid_l1.keys()))
-        print('------------------\nvalid L1')
-        for norm in valid_l1_keys:
-            valid_l1_x.append(norm)
-            valid_l1_y.append(valid_l1[norm])
-            print(f'{norm}: {valid_l1[norm]}')
-        print('------------------\nvalid L2')
-        valid_l2_keys=sorted(list(valid_l2.keys()))
-        for norm in valid_l2_keys:
-            valid_l2_x.append(norm)
-            valid_l2_y.append(valid_l2[norm])
-            print(f'{norm}: {valid_l2[norm]}')
-        print('------------------\ninvalid L1')
-        invalid_l1_keys=sorted(list(invalid_l1.keys()))
-        for norm in invalid_l1_keys:
-            invalid_l1_x.append(norm)
-            invalid_l1_y.append(invalid_l1[norm])
-            print(f'{norm}: {invalid_l1[norm]}')
-        print('------------------\ninvalid L2')
-        invalid_l2_keys=sorted(list(invalid_l2.keys()))
-        for norm in invalid_l2_keys:
-            invalid_l2_x.append(norm)
-            invalid_l2_y.append(invalid_l2[norm])
-            print(f'{norm}: {invalid_l2[norm]}')
-
-        plt.plot(valid_l1_x,valid_l1_y,'r-',label='valid patch l1')
-        plt.plot(valid_l2_x,valid_l2_y,'r--',label='valid patch l2')
-        plt.plot(invalid_l1_x,invalid_l1_y,'b-',label='invalid patch l1')
-        plt.plot(invalid_l2_x,invalid_l2_y,'b--',label='invalid patch l2')
-        plt.legend()
-        plt.savefig(f'{output_dir}/{test_name}.jpg')
-
 if __name__ == "__main__":
     if len(sys.argv)!=3:
         print('Usage: python3 plot_norm.py <output_dir> <branch_dir>')
@@ -152,4 +92,3 @@ if __name__ == "__main__":
     output_dir=sys.argv[1]
     branch_dir=sys.argv[2]
     valid_l1_norms,valid_l2_norms,invalid_l1_norms,invalid_l2_norms=compute_norms(output_dir,branch_dir)
-    plot(output_dir,valid_l1_norms,valid_l2_norms,invalid_l1_norms,invalid_l2_norms)
