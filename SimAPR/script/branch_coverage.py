0f8934debdc92d643b9c53e8cbe55056fcd2831e
FreddyYJ <kyj1411@unist.ac.kr>
2023-04-05 06:53:00 +0000

    Add T-sne to script

--- SimAPR/script/branch_coverage.py
+++ SimAPR/script/branch_coverage.py
@@ -4,6 +4,7 @@ import sys
 from typing import Dict, List, Tuple
 import numpy as np
 import matplotlib.pyplot as plt
+import sklearn.manifold
 
 
 def parse_cov(cov_file: str):
@@ -24,12 +25,6 @@ def parse_cov(cov_file: str):
 
     return np.array(covered_branches),cov
 
-def get_l1_norm(cov: np.ndarray):
-    return np.linalg.norm(cov, ord=1)
-
-def get_l2_norm(cov: np.ndarray):
-    return np.linalg.norm(cov, ord=2)
-
 def parse_simapr_result(output_file:str):
     valid_patches=[]
     invalid_patches=[]
@@ -40,50 +35,43 @@ def parse_simapr_result(output_file:str):
         patch_id=patch['config'][0]['location']
         if patch['pass_result']:
             valid_patches.append(patch_id)
-        else:
+        elif patch['compilable']:
             invalid_patches.append(patch_id)
     
     return valid_patches,invalid_patches
 
-def compute_norms(output_file:str,branch_dir:str) -> Tuple[Dict[str,Dict[float,int]],Dict[str,Dict[float,int]],Dict[str,Dict[float,int]],Dict[str,Dict[float,int]]]:
-    valid_pathces,invalid_patches=parse_simapr_result(output_file)
-    valid_l1_norms,valid_l2_norms,invalid_l1_norms,invalid_l2_norms=dict(),dict(),dict(),dict()
-    valid_l1_set,valid_l2_set=set(),set()
+def get_all_cov_info(branch_dir:str):
+    all_cov:Dict[str,Dict[int,int]]=dict()
+    original_cov:Dict[int,int]=dict()
 
     for file in os.listdir(branch_dir):
-        patch_id,test_name=file.split('.java_')
-        patch_id+='.java'
-        patch_id=patch_id.replace('#','/')
-        covered_branches,coverage=parse_cov(f'{branch_dir}/{file}')
+        if file.startswith('original'):
+            original_cov=parse_cov(f'{branch_dir}/{file}')[1]
+        else:
+            all_cov[file.removesuffix('.txt')]=parse_cov(f'{branch_dir}/{file}')[1]
 
-        l1_norm=get_l1_norm(covered_branches)
-        l2_norm=get_l2_norm(covered_branches)
+    return all_cov,original_cov
 
-        if patch_id in valid_pathces:
-            print(f'{patch_id}: L1: {l1_norm}, L2: {l2_norm}')
-            if test_name not in valid_l1_norms.keys():
-                valid_l1_norms[test_name]=dict()
-            if test_name not in valid_l2_norms.keys():
-                valid_l2_norms[test_name]=dict()
-            if l1_norm not in valid_l1_norms[test_name].keys():
-                valid_l1_norms[test_name][l1_norm]=0
-            if l2_norm not in valid_l2_norms[test_name].keys():
-                valid_l2_norms[test_name][l2_norm]=0
-            valid_l1_norms[test_name][l1_norm]+=1
-            valid_l2_norms[test_name][l2_norm]+=1
-        elif patch_id in invalid_patches:
-            if test_name not in invalid_l1_norms.keys():
-                invalid_l1_norms[test_name]=dict()
-            if test_name not in invalid_l2_norms.keys():
-                invalid_l2_norms[test_name]=dict()
-            if l1_norm not in invalid_l1_norms[test_name].keys():
-                invalid_l1_norms[test_name][l1_norm]=0
-            if l2_norm not in invalid_l2_norms[test_name].keys():
-                invalid_l2_norms[test_name][l2_norm]=0
-            invalid_l1_norms[test_name][l1_norm]+=1
-            invalid_l2_norms[test_name][l2_norm]+=1
+def tsne(cov:Dict[str,Dict[str,Dict[int,int]]],valid:List[str],invalid:List[str]):
+    x=[]
+    for test,info in cov.items():
+        for patch in valid:
+            vector=info[patch]
+            cur_x=[]
+            for branch,coverage in vector.items():
+                cur_x.append([branch,coverage])
+            x.append(cur_x)
 
-    return valid_l1_norms,valid_l2_norms,invalid_l1_norms,invalid_l2_norms
+        for patch in invalid:
+            vector=info[patch]
+            cur_x=[]
+            for branch,coverage in vector.items():
+                cur_x.append([branch,coverage])
+            x.append(cur_x)
+
+        tsne_plot=sklearn.manifold.TSNE()
+        x_2d=tsne_plot.fit_transform(np.array(x))
+        print(x_2d)
 
 if __name__ == "__main__":
     if len(sys.argv)!=3:
@@ -91,4 +79,17 @@ if __name__ == "__main__":
         exit(1)
     output_dir=sys.argv[1]
     branch_dir=sys.argv[2]
-    valid_l1_norms,valid_l2_norms,invalid_l1_norms,invalid_l2_norms=compute_norms(output_dir,branch_dir)
+
+    valid_patches,invalid_patches=parse_simapr_result(output_dir)
+    all_cov,original_cov=get_all_cov_info(branch_dir)
+
+    all_cov_test=dict()
+    for patch, vector in all_cov.items():
+        patch_id,test=patch.split('.java_')
+        patch_id+='java'
+        patch_id=patch_id.replace('#','/')
+        if test not in all_cov_test.keys():
+            all_cov_test[test]=dict()
+        all_cov_test[test][patch_id]=vector
+
+    tsne(all_cov_test['test1'],valid_patches,invalid_patches)
