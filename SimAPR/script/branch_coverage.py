3dc851c572cb377db2ed57720d24d21c83a21065
FreddyYJ <kyj1411@unist.ac.kr>
2023-04-06 03:06:14 +0000

    Add T-sne script

--- SimAPR/script/branch_coverage.py
+++ SimAPR/script/branch_coverage.py
@@ -6,17 +6,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sklearn.manifold
 
-
+__max_branch_id=0
 def parse_cov(cov_file: str):
     """
     :param cov_file: branch coverage file
     :return: branch coverage vector
     """
+    global __max_branch_id
     cov=dict()
     covered_branches=[]
     with open(cov_file, 'r') as f:
         for line in f:
             branch=int(line.strip())
+            if branch > __max_branch_id:
+                __max_branch_id = branch
             if branch in cov.keys():
                 cov[branch]+=1
             else:
@@ -50,32 +53,59 @@ def get_all_cov_info(branch_dir:str):
         else:
             all_cov[file.removesuffix('.txt')]=parse_cov(f'{branch_dir}/{file}')[1]
 
+    # Padd uncovered branches with 0
+    for file,cov in all_cov.items():
+        for branch in range(__max_branch_id+1):
+            if branch not in cov.keys():
+                cov[branch]=0
+    for branch in range(__max_branch_id+1):
+        if branch not in original_cov.keys():
+            original_cov[branch]=0
+
     return all_cov,original_cov
 
-def tsne(cov:Dict[str,Dict[str,Dict[int,int]]],valid:List[str],invalid:List[str]):
+def tsne(cov:Dict[str,Dict[str,Dict[int,int]]],valid:List[str],invalid:List[str],out_dir:str):
     x=[]
     for test,info in cov.items():
         for patch in valid:
+            if patch not in info.keys():
+                continue
             vector=info[patch]
+            branches=list(vector.keys())
+            sorted_branches=sorted(branches)
             cur_x=[]
-            for branch,coverage in vector.items():
-                cur_x.append([branch,coverage])
+            for branch in sorted_branches:
+                cur_x.append(vector[branch])
             x.append(cur_x)
 
         for patch in invalid:
+            if patch not in info.keys():
+                continue
             vector=info[patch]
+            branches=list(vector.keys())
+            sorted_branches=sorted(branches)
             cur_x=[]
-            for branch,coverage in vector.items():
-                cur_x.append([branch,coverage])
+            for branch in sorted_branches:
+                cur_x.append(vector[branch])
             x.append(cur_x)
 
         tsne_plot=sklearn.manifold.TSNE()
-        x_2d=tsne_plot.fit_transform(np.array(x))
-        print(x_2d)
+        x_array=np.array(x)
+        print(x_array.shape)
+        x_2d=tsne_plot.fit_transform(x_array)
+        # print(x_2d)
+        print(x_2d.shape)
+
+        plt.clf()
+        print(len(valid))
+        plt.scatter(x_2d[len(valid):,0],x_2d[len(valid):,1],c='b',label='invalid')
+        plt.scatter(x_2d[:len(valid),0],x_2d[:len(valid),1],c='r',label='valid')
+        plt.legend()
+        plt.savefig(f'{out_dir}/{test}.jpg')
 
 if __name__ == "__main__":
     if len(sys.argv)!=3:
-        print('Usage: python3 plot_norm.py <output_dir> <branch_dir>')
+        print('Usage: python3 branch_coverage.py <output_dir> <branch_dir>')
         exit(1)
     output_dir=sys.argv[1]
     branch_dir=sys.argv[2]
@@ -86,10 +116,10 @@ if __name__ == "__main__":
     all_cov_test=dict()
     for patch, vector in all_cov.items():
         patch_id,test=patch.split('.java_')
-        patch_id+='java'
+        patch_id+='.java'
         patch_id=patch_id.replace('#','/')
         if test not in all_cov_test.keys():
             all_cov_test[test]=dict()
         all_cov_test[test][patch_id]=vector
 
-    tsne(all_cov_test['test1'],valid_patches,invalid_patches)
+    tsne(all_cov_test,valid_patches,invalid_patches,output_dir)
