031867b41fb306c4cb3bd8f368e8e9a98a5f37d7
FreddyYJ <kyj1411@unist.ac.kr>
2023-05-03 05:00:16 +0000

    Update experiment scripts

--- experiments/recoder/search-recoder-casino.py
+++ experiments/recoder/search-recoder-casino.py
@@ -16,20 +16,20 @@ def run(project,seed):
     for dir in cur_dirs[:-2]:
         new_cur_dir+=dir+'/'
 
-    print(f"Run {project}-simapr")
-    result=subprocess.run(['python3',f'{new_cur_dir}/SimAPR/simapr.py','-o',f'result/{project}-simapr','-m','casino','--seed',f'{seed}',
+    print(f"Run {project}-casino")
+    result=subprocess.run(['python3',f'{new_cur_dir}/SimAPR/simapr.py','-o',f'result/{project}-casino','-m','casino','--seed',f'{seed}',
                 '-k','learning','-w',f'{new_cur_dir}/Recoder/d4j/{project}','-t','180000','--use-simulation-mode',f'result/cache/{project}-cache.json',
                 '--instr-cp','../../../JPatchInst','--branch-output',f'result/branch/{project}',
                 '-T','18000', '--','python3',
                 f'{new_cur_dir}/SimAPR/script/d4j_run_test.py',f'{new_cur_dir}/Recoder/buggy'],stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
     
     print(result.stdout.decode('utf-8'))
-    print(f'{project} simapr finish with return code {result.returncode}')
+    print(f'{project} casino finish with return code {result.returncode}')
 
 if __name__ == '__main__':
     args=sys.argv
     if len(args)!=3:
-        print('Usage: python3 search-recoder-simapr.py <project> <seed>')
+        print('Usage: python3 search-recoder-casino.py <project> <seed>')
         sys.exit(1)
     
     run(args[1],args[2])
--- experiments/tbar/search-tbar-greybox.py
+++ experiments/tbar/search-tbar-greybox.py
@@ -14,20 +14,20 @@ def run(project,seed):
     for dir in cur_dirs[:-2]:
         new_cur_dir+=dir+'/'
 
-    print(f"Run {project}-simapr")
-    result=subprocess.run(['python3',f'{new_cur_dir}/SimAPR/simapr.py','-o',f'result/{project}-simapr','-m','greybox','--seed',f'{seed}',
+    print(f"Run {project}-greybox")
+    result=subprocess.run(['python3',f'{new_cur_dir}/SimAPR/simapr.py','-o',f'result/{project}-greybox','-m','greybox','--seed',f'{seed}',
                 '-k','template','-w',f'{new_cur_dir}/TBar/d4j/{project}','-t','180000','--use-simulation-mode',f'result/cache/{project}-cache.json',
                 '--instr-cp','../../../JPatchInst','--branch-output',f'result/branch/{project}',
                 '-T','18000', '--','python3',
                 f'{new_cur_dir}/SimAPR/script/d4j_run_test.py',f'{new_cur_dir}/TBar/buggy'])
     
-    print(f'{project} simapr finish with return code {result.returncode}')
+    print(f'{project} greybox finish with return code {result.returncode}')
     exit(result.returncode)
 
 if __name__ == '__main__':
     args=sys.argv
     if len(args)!=3:
-        print('Usage: python3 search-tbar-simapr.py <project> <seed>')
+        print('Usage: python3 search-tbar-greybox.py <project> <seed>')
         sys.exit(1)
     
     run(args[1],args[2])
