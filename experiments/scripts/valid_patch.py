a61a6ccc88621b230b03a45d44a04547761638b7
FreddyYJ <kyj1411@unist.ac.kr>
2023-05-16 04:42:14 +0000

    Add scripts to generate plots

---
+++ experiments/scripts/valid_patch.py
@@ -0,0 +1,147 @@
+import json
+import os
+import sys
+from typing import Dict, List
+
+from benchmarks import *
+
+MAX_EXECUTION=10
+
+def get_result(output_file:str):
+    try:
+        result_file=open(f'{output_file}/simapr-result.json','r')
+    except:
+        print(f'Error: {output_file}/simapr-result.json does not exist')
+        sys.exit(1)
+        
+    root=json.load(result_file)
+    result_file.close()
+
+    prev_time=0.
+    hq_result:List[int]=[]
+    valid_result:List[int]=[]
+    for res in root:
+        is_hq=res['result']
+        is_plausible=res['pass_result']
+        iteration=res['iteration']
+        time=res['time']
+        loc=res['config'][0]['location']
+
+        if is_hq:
+            hq_result.append((iteration,round(time/60)))
+        if is_plausible:
+            valid_result.append((iteration,round(time/60)))
+
+    return hq_result,valid_result
+
+def valid_patch_each_project(result_dir:str,max_exp:int,use_casino:bool=True,use_greybox:bool=True,
+                             use_orig:bool=True,use_seapr:bool=True):
+    casino_result:Dict[str,List[int]]=dict()  # {project: [valid_patches...]}
+    if use_casino:
+        for project in D4J_1_2_0_LIST:
+            for i in range(max_exp):
+                result_path=f'{result_dir}/{project}-casino-{i}/simapr-result.json'
+                if os.path.exists(result_path):
+                    hq_result,valid_result=get_result(result_path)
+                    
+                    if project not in casino_result:
+                        casino_result[project]=[]
+                    casino_result[project]+=valid_result
+
+    greybox_result:Dict[str,List[int]]=dict()  # {project: [valid_patches...]}
+    if use_greybox:
+        for project in D4J_1_2_0_LIST:
+            for i in range(max_exp):
+                result_path=f'{result_dir}/{project}-greybox-{i}/simapr-result.json'
+                if os.path.exists(result_path):
+                    hq_result,valid_result=get_result(result_path)
+                    
+                    if project not in greybox_result:
+                        greybox_result[project]=[]
+                    greybox_result[project]+=valid_result
+
+    original_result:Dict[str,List[int]]=dict()  # {project: [valid_patches...]}
+    if use_orig:
+        for project in D4J_1_2_0_LIST:
+            result_path=f'{result_dir}/{project}-orig/simapr-result.json'
+            if os.path.exists(result_path):
+                hq_result,valid_result=get_result(result_path)
+                
+                if project not in original_result:
+                    original_result[project]=[]
+                original_result[project]+=valid_result
+
+    seapr_result:Dict[str,List[int]]=dict()  # {project: [valid_patches...]}
+    if use_seapr:
+        for project in D4J_1_2_0_LIST:
+            result_path=f'{result_dir}/{project}-seapr/simapr-result.json'
+            if os.path.exists(result_path):
+                hq_result,valid_result=get_result(result_path)
+                
+                if project not in seapr_result:
+                    seapr_result[project]=[]
+                seapr_result[project]+=valid_result
+
+    final_result:Dict[str,Dict[str,List[int]]]=dict()  # {algorithm: {project: [valid_patches...]}}
+    if use_casino:
+        final_result['casino']=casino_result
+    if use_greybox:
+        final_result['greybox']=greybox_result
+    if use_orig:
+        final_result['orig']=original_result
+    if use_seapr:
+        final_result['seapr']=seapr_result
+    
+    return final_result
+
+def valid_patch_combine(result_dir:str,max_exp:int,use_casino:bool=True,use_greybox:bool=True,
+                             use_orig:bool=True,use_seapr:bool=True):
+    casino_result:List[int]=[]  # [valid_patches...]
+    if use_casino:
+        for project in D4J_1_2_0_LIST:
+            for i in range(max_exp):
+                result_path=f'{result_dir}/{project}-casino-{i}/simapr-result.json'
+                if os.path.exists(result_path):
+                    hq_result,valid_result=get_result(result_path)
+                    
+                    casino_result+=valid_result
+
+    greybox_result:List[int]=[]  # [valid_patches...]
+    if use_greybox:
+        for project in D4J_1_2_0_LIST:
+            for i in range(max_exp):
+                result_path=f'{result_dir}/{project}-greybox-{i}/simapr-result.json'
+                if os.path.exists(result_path):
+                    hq_result,valid_result=get_result(result_path)
+                    
+                    greybox_result+=valid_result
+
+    original_result:List[int]=[]  # [valid_patches...]
+    if use_orig:
+        for project in D4J_1_2_0_LIST:
+            result_path=f'{result_dir}/{project}-orig/simapr-result.json'
+            if os.path.exists(result_path):
+                hq_result,valid_result=get_result(result_path)
+                
+                original_result+=valid_result
+
+    seapr_result:List[int]=[]  # [valid_patches...]
+    if use_seapr:
+        for project in D4J_1_2_0_LIST:
+            result_path=f'{result_dir}/{project}-seapr/simapr-result.json'
+            if os.path.exists(result_path):
+                hq_result,valid_result=get_result(result_path)
+                
+                seapr_result+=valid_result
+
+    final_result:Dict[str,List[int]]=dict()  # {algorithm: [valid_patches...]}
+    if use_casino:
+        final_result['casino']=casino_result
+    if use_greybox:
+        final_result['greybox']=greybox_result
+    if use_orig:
+        final_result['orig']=original_result
+    if use_seapr:
+        final_result['seapr']=seapr_result
+    
+    return final_result
---
+++ experiments/scripts/valid_patch_overall_plot.py
@@ -0,0 +1,51 @@
+from sys import argv
+import matplotlib.pyplot as plt
+
+import valid_patch
+
+if __name__=='__main__':
+    if len(argv)<4:
+        print(f'Usage: valid_patch_overall_plot.py <result_dir> <max_exp> <algorithms...>')
+        exit(1)
+    
+    result_dir=argv[1]
+    max_exp=int(argv[2])
+    algorithms=argv[3:]
+    
+    result=valid_patch.valid_patch_combine(result_dir,max_exp,use_casino=('casino' in algorithms),
+                                                use_greybox=('greybox' in algorithms),use_orig=('orig' in algorithms),
+                                                use_seapr=('seapr' in algorithms))
+    
+    sorted_algorithm=[]
+    if 'greybox' in algorithms:
+        sorted_algorithm.append('greybox')
+    if 'casino' in algorithms:
+        sorted_algorithm.append('casino')
+    if 'seapr' in algorithms:
+        sorted_algorithm.append('seapr')
+    if 'orig' in algorithms:
+        sorted_algorithm.append('orig')
+    
+    COLORS=['r','g','b','y']
+    TIMEOUT=5*60
+    plt.clf()
+    for algorithm in sorted_algorithm:
+        print(f'Algorithm: {algorithm}')
+        current_result=sorted(result[algorithm])
+
+        y=[]
+        for i in range(TIMEOUT):
+            if i in current_result:
+                if algorithm in ('casino','greybox'):
+                    y.append(y[-1]+current_result.count(i)/max_exp)
+                else:
+                    y.append(y[-1]+current_result.count(i))
+            else:
+                y.append(y[-1])
+        
+        plt.plot(range(TIMEOUT),y,color=COLORS.pop(0),label=algorithm)
+    
+    plt.legend()
+    plt.xlabel('Time (min)')
+    plt.ylabel('# of Valid patches')
+    plt.savefig(f'valid_patch_overall_plot.png')
