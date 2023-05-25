2dbb39f298a2528d13ec89b57b159d65a9d2a368
FreddyYJ <kyj1411@unist.ac.kr>
2023-05-25 01:28:08 +0000

    Fix plot script

--- experiments/scripts/rq1.py
+++ experiments/scripts/rq1.py
@@ -174,7 +174,7 @@ def plot_patches_ci_java(mode='tbar'):
     guided_x=[]
     guided_y=[]
     temp_=[[],[],[],[],[]]
-    for j in range(50):
+    for j in range(10):
         cur_result=sorted(casino_result[j])
         guided_list.append([0])
         for i in range(0,300):
@@ -193,40 +193,59 @@ def plot_patches_ci_java(mode='tbar'):
     for i in range(5):
         print(f'{i*60}: {np.std(temp_[i])}')
 
-    # SeAPR
-    results=sorted(seapr_result)
-    other_list=[0]
-    for i in range(0,300):
-        if i in results:
-            other_list.append(other_list[-1]+results.count(i))
-        else:
-            other_list.append(other_list[-1])
-    plt.plot(list(range(0,301)),other_list,':g',label='SeAPR')
-
-    # GenProg
-    other_list:List[List[int]]=[]
-    other_x=[]
-    other_y=[]
-    for j in range(50):
-        cur_result=sorted(genprog_result[j])
-        other_list.append([0])
+    # Greybox
+    guided_list:List[List[int]]=[]
+    guided_x=[]
+    guided_y=[]
+    for j in range(10):
+        cur_result=sorted(casino_result[j])
+        guided_list.append([0])
         for i in range(0,300):
             if i in cur_result:
-                other_list[-1].append((50*other_list[-1][-1]+cur_result.count(i))/50)
-                other_x.append(i)
-                other_y.append((50*other_list[-1][-1]+cur_result.count(i))/50)
+                guided_list[-1].append((50*guided_list[-1][-1]+cur_result.count(i))/50)
+                guided_x.append(i)
+                guided_y.append((50*guided_list[-1][-1]+cur_result.count(i))/50)
             else:
-                other_list[-1].append(other_list[-1][-1])
-                other_x.append(i)
-                other_y.append(other_list[-1][-1])
-    other_df=pd.DataFrame({'Time':other_x,'Number of valid patches':other_y})
-    seaborn.lineplot(data=other_df,x='Time',y='Number of valid patches',color='y',label='GenProg',linestyle='dashed')
-    plt.legend(fontsize=12)
-    plt.xlabel('Time (min)',fontsize=15)
-    plt.ylabel('# of Valid Patches',fontsize=15)
-    plt.xticks(fontsize=15)
-    plt.yticks(fontsize=15)
-    plt.savefig(f'rq1-{mode}.pdf',bbox_inches='tight')
+                guided_list[-1].append(guided_list[-1][-1])
+                guided_x.append(i)
+                guided_y.append(guided_list[-1][-1])
+    guided_df=pd.DataFrame({'Time':guided_x,'Number of valid patches':guided_y})
+    seaborn.lineplot(data=guided_df,x='Time',y='Number of valid patches',color='g',label='Greybox')
+
+    # # SeAPR
+    # results=sorted(seapr_result)
+    # other_list=[0]
+    # for i in range(0,300):
+    #     if i in results:
+    #         other_list.append(other_list[-1]+results.count(i))
+    #     else:
+    #         other_list.append(other_list[-1])
+    # plt.plot(list(range(0,301)),other_list,':g',label='SeAPR')
+
+    # # GenProg
+    # other_list:List[List[int]]=[]
+    # other_x=[]
+    # other_y=[]
+    # for j in range(50):
+    #     cur_result=sorted(genprog_result[j])
+    #     other_list.append([0])
+    #     for i in range(0,300):
+    #         if i in cur_result:
+    #             other_list[-1].append((50*other_list[-1][-1]+cur_result.count(i))/50)
+    #             other_x.append(i)
+    #             other_y.append((50*other_list[-1][-1]+cur_result.count(i))/50)
+    #         else:
+    #             other_list[-1].append(other_list[-1][-1])
+    #             other_x.append(i)
+    #             other_y.append(other_list[-1][-1])
+    # other_df=pd.DataFrame({'Time':other_x,'Number of valid patches':other_y})
+    # seaborn.lineplot(data=other_df,x='Time',y='Number of valid patches',color='y',label='GenProg',linestyle='dashed')
+    # plt.legend(fontsize=12)
+    # plt.xlabel('Time (min)',fontsize=15)
+    # plt.ylabel('# of Valid Patches',fontsize=15)
+    # plt.xticks(fontsize=15)
+    # plt.yticks(fontsize=15)
+    # plt.savefig(f'rq1-{mode}.pdf',bbox_inches='tight')
 
 plot_patches_ci_java('tbar')
 plot_patches_ci_java('avatar')
