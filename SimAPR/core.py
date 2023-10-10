edbc3909f68f3ee35fd9079833d8b15bf4fe6900
FreddyYJ <kyj1411@unist.ac.kr>
2023-10-10 02:14:20 +0000

    Update json format for learning based tools

--- SimAPR/core.py
+++ SimAPR/core.py
@@ -248,8 +248,6 @@ class RecoderCaseInfo(PatchTreeNode):
     return hash(self.location)
   def __eq__(self, other) -> bool:
     return self.location == other.location
-  def to_str(self) -> str:
-    return f"{self.parent.line_id}-{self.case_id}"
 
 # Find with f"{file_name}:{line_number}"
 class FileLine:
@@ -358,7 +356,6 @@ class EnvGenerator:
     new_env["SIMAPR_BUGGY_PROJECT"] = state.d4j_buggy_project
     new_env["SIMAPR_OUTPUT_DISTANCE_FILE"] = f"/tmp/{uuid.uuid4()}.out"
     new_env["SIMAPR_TIMEOUT"] = str(state.timeout)
-    new_env["SIMAPR_RECODER"] = "-"
 
     if state.mode==Mode.greybox:
       new_env['GREYBOX_BRANCH']='1'
@@ -473,9 +470,9 @@ class RecoderPatchInfo:
     self.func_info.positive_pf.update(result, n,b_n, exp_alpha)
     self.file_info.positive_pf.update(result, n,b_n, exp_alpha)
   def remove_patch(self, state: 'GlobalState') -> None:
-    if self.recoder_case_info.case_id not in self.line_info.recoder_case_info_map:
-      state.logger.critical(f"{self.recoder_case_info.case_id} not in {self.line_info.recoder_case_info_map}")
-    del self.line_info.recoder_case_info_map[self.recoder_case_info.case_id]
+    if self.recoder_case_info.location not in self.line_info.recoder_case_info_map:
+      state.logger.critical(f"{self.recoder_case_info.location} not in {self.line_info.recoder_case_info_map}")
+    del self.line_info.recoder_case_info_map[self.recoder_case_info.location]
 
     if len(self.line_info.recoder_case_info_map) == 0:
       score = self.line_info.fl_score
@@ -525,7 +522,7 @@ class RecoderPatchInfo:
   def __str__(self) -> str:
     return self.to_str()
   def to_str_sw_cs(self) -> str:
-    return f"{self.line_info.line_id}-{self.recoder_case_info.case_id}"
+    return self.to_str()
   @staticmethod
   def list_to_str(selected_patch: list) -> str:
     result = list()
