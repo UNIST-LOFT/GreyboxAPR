c17e15d9beb3cb9fd54dd0aec9679d23adbfefee
eumppe <pych05@unist.ac.kr>
2024-01-16 00:54:50 +0000

    automatic-runner-update

--- yechan_excute_all_in_one.sh
+++ yechan_excute_all_in_one.sh
@@ -5,22 +5,22 @@ cd /root/project/SimAPR
 
 for element in "${my_array[@]}"; do
     #run original once
-    echo "executing: python3 SimAPR/simapr.py -o experiments/yechan/tbar/result/$element-orig-out -m orig --instr-cp /root/project/JPatchInst -w /root/project/SimAPR/TBar/d4j/$element -t 36000 -T 18000 --seed 1 --use-simulation-mode /root/project/SimAPR/experiments/tbar/result/cache/$element.json -k template --skip-valid -- python3 /root/project/SimAPR/SimAPR/script/d4j_run_test.py /root/project/SimAPR/TBar/buggy"
-    python3 SimAPR/simapr.py -o experiments/yechan/tbar/result/$element-orig-out -m orig --instr-cp /root/project/JPatchInst -w /root/project/SimAPR/TBar/d4j/$element -t 36000 -T 18000 --seed 1 --use-simulation-mode /root/project/SimAPR/experiments/tbar/result/cache/$element.json -k template --skip-valid -- python3 /root/project/SimAPR/SimAPR/script/d4j_run_test.py /root/project/SimAPR/TBar/buggy
+    echo "executing: python3 SimAPR/simapr.py -o experiments/yechan/tbar/result/$element-orig-out -m orig --instr-cp /root/project/JPatchInst -w /root/project/SimAPR/TBar/d4j/$element -t 36000 -T 18000 --seed 1 --use-simulation-mode /root/project/SimAPR/experiments/tbar/result/cache/$element-cache.json -k template --skip-valid -- python3 /root/project/SimAPR/SimAPR/script/d4j_run_test.py /root/project/SimAPR/TBar/buggy"
+    python3 SimAPR/simapr.py -o experiments/yechan/tbar/result/$element-orig-out -m orig --instr-cp /root/project/JPatchInst -w /root/project/SimAPR/TBar/d4j/$element -t 36000 -T 18000 --seed 1 --use-simulation-mode /root/project/SimAPR/experiments/tbar/result/cache/$element-cache.json -k template --skip-valid -- python3 /root/project/SimAPR/SimAPR/script/d4j_run_test.py /root/project/SimAPR/TBar/buggy
     
     # run casino 10 times
     for i in {1..10}; do
         echo "casino running: $i"
-        echo "executing: python3 SimAPR/simapr.py -o experiments/yechan/tbar/result/$element-casino-$i-out -m casino --instr-cp /root/project/JPatchInst -w /root/project/SimAPR/TBar/d4j/$element -t 36000 -T 18000 --seed 1 --use-simulation-mode /root/project/SimAPR/experiments/tbar/result/cache/$element.json -k template --skip-valid -- python3 /root/project/SimAPR/SimAPR/script/d4j_run_test.py /root/project/SimAPR/TBar/buggy"
+        echo "executing: python3 SimAPR/simapr.py -o experiments/yechan/tbar/result/$element-casino-$i-out -m casino --instr-cp /root/project/JPatchInst -w /root/project/SimAPR/TBar/d4j/$element -t 36000 -T 18000 --seed 1 --use-simulation-mode /root/project/SimAPR/experiments/tbar/result/cache/$element-cache.json -k template --skip-valid -- python3 /root/project/SimAPR/SimAPR/script/d4j_run_test.py /root/project/SimAPR/TBar/buggy"
 
-        python3 SimAPR/simapr.py -o experiments/yechan/tbar/result/$element-casino-$i-out -m casino --instr-cp /root/project/JPatchInst -w /root/project/SimAPR/TBar/d4j/$element -t 36000 -T 18000 --seed $i --use-simulation-mode /root/project/SimAPR/experiments/tbar/result/cache/$element.json -k template --skip-valid -- python3 /root/project/SimAPR/SimAPR/script/d4j_run_test.py /root/project/SimAPR/TBar/buggy
+        python3 SimAPR/simapr.py -o experiments/yechan/tbar/result/$element-casino-$i-out -m casino --instr-cp /root/project/JPatchInst -w /root/project/SimAPR/TBar/d4j/$element -t 36000 -T 18000 --seed $i --use-simulation-mode /root/project/SimAPR/experiments/tbar/result/cache/$element-cache.json -k template --skip-valid -- python3 /root/project/SimAPR/SimAPR/script/d4j_run_test.py /root/project/SimAPR/TBar/buggy
     done
 
     # run greybox 10 times
     for i in {1..10}; do
         echo "greybox running: $i"
-        echo "executing: python3 SimAPR/simapr.py -o experiments/yechan/tbar/result/$element-greybox-$i-out -m greybox --instr-cp /root/project/JPatchInst -w /root/project/SimAPR/TBar/d4j/$element -t 36000 -T 18000 --seed $i --use-simulation-mode /root/project/SimAPR/experiments/tbar/result/cache/$element.json -k template --skip-valid -- python3 /root/project/SimAPR/SimAPR/script/d4j_run_test.py /root/project/SimAPR/TBar/buggy"
+        echo "executing: python3 SimAPR/simapr.py -o experiments/yechan/tbar/result/$element-greybox-$i-out -m greybox --instr-cp /root/project/JPatchInst -w /root/project/SimAPR/TBar/d4j/$element -t 36000 -T 18000 --seed $i --use-simulation-mode /root/project/SimAPR/experiments/tbar/result/cache/$element-cache.json -k template --skip-valid -- python3 /root/project/SimAPR/SimAPR/script/d4j_run_test.py /root/project/SimAPR/TBar/buggy"
 
-        python3 SimAPR/simapr.py -o experiments/yechan/tbar/result/$element-greybox-$i-out -m greybox --instr-cp /root/project/JPatchInst -w /root/project/SimAPR/TBar/d4j/$element -t 36000 -T 18000 --seed $i --use-simulation-mode /root/project/SimAPR/experiments/tbar/result/cache/$element.json -k template --skip-valid -- python3 /root/project/SimAPR/SimAPR/script/d4j_run_test.py /root/project/SimAPR/TBar/buggy
+        python3 SimAPR/simapr.py -o experiments/yechan/tbar/result/$element-greybox-$i-out -m greybox --instr-cp /root/project/JPatchInst -w /root/project/SimAPR/TBar/d4j/$element -t 36000 -T 18000 --seed $i --use-simulation-mode /root/project/SimAPR/experiments/tbar/result/cache/$element-cache.json -k template --skip-valid -- python3 /root/project/SimAPR/SimAPR/script/d4j_run_test.py /root/project/SimAPR/TBar/buggy
     done
 done
