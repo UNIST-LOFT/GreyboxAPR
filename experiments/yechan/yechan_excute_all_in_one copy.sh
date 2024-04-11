4ecd4c582725273c7a409be91eaece0c645052c9
eumppe <pych05@unist.ac.kr>
2024-04-11 08:38:33 +0000

    Fix some directory

--- experiments/yechan/yechan_excute_all_in_one copy.sh
+++
@@ -1,8 +0,0 @@
-
-# run greybox 10 times
-for i in {1..38}; do
-    echo "greybox running: $i"
-    echo "executing: python3 SimAPR/simapr.py -o experiments/yechan/tbar/Mockito_result/Mockito_$i-greybox-out -m greybox --instr-cp /root/project/JPatchInst -w /root/project/SimAPR/TBar/d4j/Mockito_$i -t 36000 -T 18000 --seed $i --use-simulation-mode /root/project/SimAPR/experiments/tbar/result/cache/Mockito_$i-cache.json -k template --skip-valid -- python3 /root/project/SimAPR/SimAPR/script/d4j_run_test.py /root/project/SimAPR/TBar/buggy"
-
-    python3 SimAPR/simapr.py -o experiments/yechan/tbar/Mockito_result/Mockito_$i-greybox-$i-out -m greybox --instr-cp /root/project/JPatchInst --branch-output experiments/tbar/Mockito_result/branch/Mockito_$i -w /root/project/SimAPR/TBar/d4j/Mockito_$i -t 36000 -T 18000 --seed $i --use-simulation-mode /root/project/SimAPR/experiments/tbar/result/cache/Mockito_$i-cache.json -k template --skip-valid -- python3 /root/project/SimAPR/SimAPR/script/d4j_run_test.py /root/project/SimAPR/TBar/buggy
-done
