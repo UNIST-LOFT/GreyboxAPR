c1c3cbd345f89f75acd96a85b94c09f00d312d87
FreddyYJ <kyj1411@unist.ac.kr>
2024-05-13 08:14:46 +0000

    Add SRepair compile script

---
+++ SRepair/compile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir buggy
+mkdir d4j
+
+cd DatasetExtractor
+./gradlew shadowJar
+cd ..
+
+pip3 install psutil tqdm tiktoken openai
+pip3 install git+https://github.com/huggingface/transformers.git@main
+
+python3 pull_models.py
