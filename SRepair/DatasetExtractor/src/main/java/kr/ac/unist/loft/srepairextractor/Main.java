77605aca1c1556f55fff007801d6cfbb35385bc2
FreddyYJ <kyj1411@unist.ac.kr>
2024-05-13 02:48:46 +0000

    Add getting location infos

--- SRepair/DatasetExtractor/src/main/java/kr/ac/unist/loft/srepairextractor/Main.java
+++ SRepair/DatasetExtractor/src/main/java/kr/ac/unist/loft/srepairextractor/Main.java
@@ -1,9 +1,12 @@
 package kr.ac.unist.loft.srepairextractor;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -11,8 +14,12 @@ import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
 import com.google.common.base.Charsets;
 import com.google.common.io.Files;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 public class Main {
     public static void main(String[] args) {
@@ -45,7 +52,44 @@ public class Main {
                 locations.add(loc);
             }
 
-            
+            // Compile suitable source files
+            Map<String,CompilationUnit> cuMap=new HashMap<>();
+            for (Location loc:locations){
+                String file=loc.file;
+                if (!cuMap.containsKey(file)){
+                    File srcFile = new File(file);
+                    if (!srcFile.exists()) {
+                        System.exit(1);
+                    }
+                    CompilationUnit cu = StaticJavaParser.parse(new FileReader(srcFile));
+                    cuMap.put(file, cu);
+                }
+            }
+
+            // Read failing tests from SRepair dataset
+            String datasetPath=arguments[1];
+            String project=arguments[2];
+            JsonObject root=JsonParser.parseReader(new FileReader(datasetPath+"/defects4j-sf.json")).getAsJsonObject();
+            JsonObject failingTests=null;
+            if (root.has(project.replace('_', '-'))) {
+                JsonObject projectObj = root.getAsJsonObject(project.replace('_', '-'));
+                failingTests = projectObj.getAsJsonObject("trigger_test");
+            }
+            else {
+                root=JsonParser.parseReader(new FileReader(datasetPath+"/defects4j-mf.json")).getAsJsonObject();
+                JsonObject projectObj=root.getAsJsonObject(project.replace('_', '-'));
+                failingTests=projectObj.getAsJsonObject("trigger_test");
+            }
+
+            // Get location infos
+            Map<Location, LocationInfo> locationInfoMap=new HashMap<>();
+            for (Map.Entry<String,CompilationUnit> entry:cuMap.entrySet()){
+                String file=entry.getKey();
+                CompilationUnit cu=entry.getValue();
+                MethodInfoVisitor methodInfoVisitor=new MethodInfoVisitor(locations, file, failingTests);
+                methodInfoVisitor.visit(cu, null);
+                locationInfoMap.putAll(methodInfoVisitor.getLocationInfoMap());
+            }
         } catch (IOException e) {
             e.printStackTrace();
             System.exit(1);
