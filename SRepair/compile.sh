c8405898245d11122b8c2622664e70f9fb250b57
FreddyYJ <kyj1411@unist.ac.kr>
2024-05-13 08:59:51 +0000

    Add pytorch dependency

--- SRepair/compile.sh
+++ SRepair/compile.sh
@@ -8,6 +8,7 @@ cd DatasetExtractor
 cd ..
 
 pip3 install psutil tqdm tiktoken openai
+pip3 install torch torchvision torchaudio
 pip3 install git+https://github.com/huggingface/transformers.git@main
 
 python3 pull_models.py
