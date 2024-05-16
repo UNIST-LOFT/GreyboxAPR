f06fc824d8bc51712d0d3805734e10aa891c8bef
FreddyYJ <kyj1411@unist.ac.kr>
2024-05-16 02:52:25 +0000

    Add both time and iteration option

--- experiments/fixminer/search-fixminer-seapr.py
+++ experiments/fixminer/search-fixminer-seapr.py
@@ -18,7 +18,7 @@ def run(project):
     result=subprocess.run(['python3',f'{new_cur_dir}/SimAPR/simapr.py','-o',f'result/{project}-seapr','-m','seapr',
                            '--ignore-compile-error','--use-pattern','-k','template','--skip-valid',
                            '-w',f'{new_cur_dir}/Fixminer/d4j/{project}','-t','180000',
-                           '--use-simulation-mode',f'result/cache/{project}-cache.json','-E','3000',
+                           '--use-simulation-mode',f'result/cache/{project}-cache.json','-E','3000','-T','18000',
                            '--','python3',f'{new_cur_dir}/SimAPR/script/d4j_run_test.py',f'{new_cur_dir}/Fixminer/buggy'])
     
     print(f'{project} seapr finish with return code {result.returncode}')
