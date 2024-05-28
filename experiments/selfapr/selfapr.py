4cf3b817d66244df8725ea0c6ad7f8785c6de155
hsh814 <shhan@unist.ac.kr>
2024-05-28 08:59:29 +0000

    Fix selfapr.py

--- experiments/selfapr/selfapr.py
+++ experiments/selfapr/selfapr.py
@@ -13,17 +13,26 @@ def main(argv):
     new_env=os.environ.copy()
     os.makedirs("d4j", exist_ok=True)
     print(f"Run SelfAPR: {bugid}")
-    cmd = f"conda run -n selfapr python3 0_localize_fault.py {bugid}"
-    result = subprocess.run(cmd, shell=True, executable='/bin/bash')
-    cmd = f"conda run -n selfapr python3 1_bug_representation.py {bugid}"
-    result = subprocess.run(cmd, shell=True, executable='/bin/bash')
-    if len(argv) > 2:
-        new_env["CUDA_VISIBLE_DEVICES"]=argv[2]
-    cmd = f"conda run -n selfapr python3 2_test.py {bugid}"
-    result=subprocess.run(cmd, env=new_env, shell=True, executable='/bin/bash')
-    print(f"SelfAPR finished raw patch gen {bugid} with return code {result.returncode}")
-    cmd = f"conda run -n selfapr python3 3_evaluate_patch.py {bugid}"
-    result = subprocess.run(cmd, shell=True, executable='/bin/bash')
+    opt = "all" 
+    if len(argv) > 3:
+        opt = argv[3]
+    if opt == "all" or opt == "pre":
+        cmd = f"conda run -n selfapr python3 0_localize_fault.py {bugid}"
+        result = subprocess.run(cmd, shell=True, executable='/bin/bash')
+        cmd = f"conda run -n selfapr python3 1_bug_representation.py {bugid}"
+        result = subprocess.run(cmd, shell=True, executable='/bin/bash')
+    
+    if opt == "all" or opt == "gpu":
+        if len(argv) > 2:
+            new_env["CUDA_VISIBLE_DEVICES"]=argv[2]
+        cmd = f"conda run -n selfapr python3 2_test.py {bugid}"
+        result=subprocess.run(cmd, env=new_env, shell=True, executable='/bin/bash')
+        print(f"SelfAPR finished raw patch gen {bugid} with return code {result.returncode}")
+    
+    if opt == "all" or opt == "post":
+        cmd = f"conda run -n selfapr python3 3_evaluate_patch.py {bugid}"
+        result = subprocess.run(cmd, shell=True, executable='/bin/bash')
+    
     exit(result.returncode)
 
 if __name__ == "__main__":
