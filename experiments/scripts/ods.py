8b579910ae9b9841e10028599e7878ad516497c5
FreddyYJ <kyj1411@unist.ac.kr>
2024-05-28 08:46:45 +0000

    Fix ODS script

--- experiments/scripts/ods.py
+++ experiments/scripts/ods.py
@@ -17,16 +17,16 @@ def run(tool:str):
 
     print(f'Running ODS with {tool}...')
     result=subprocess.run(['python3','parse_msv.py','--msv_results_path',f'../experiments/{tool}/result','--patches_path',f'../{tool_capital}/d4j',
-                            '--buggy_projects_path',f'../{tool_capital}/buggy','--output',f'../experiments/{tool}/result','--coming_tool_path','.'],cwd='../../ODS',
+                            '--buggy_projects_path',f'../{tool_capital}/buggy','--output',f'../experiments/{tool}/result','--coming_tool_path','.'],cwd='../ODS',
                             stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
     
+    print(result.stdout.decode('utf-8'))
     if result.returncode!=0:
         print(f'Error running ODS with {tool}!')
-        print(result.stdout.decode('utf-8'))
         return
     
     # Copy output to scripts folder
-    shutil.copyfile(f'../{tool}/result/prediction.csv',f'ods-{tool}.csv')
+    shutil.copyfile(f'{tool}/result/prediction.csv',f'ods-{tool}.csv')
 
     print(f'Finished ODS with {tool} with return code {result.returncode}!')
 
