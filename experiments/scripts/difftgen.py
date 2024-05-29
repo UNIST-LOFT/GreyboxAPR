4cccebc076ade66b3c2cf1d36fcd6b1c4e85061c
hsh814 <shhan@unist.ac.kr>
2024-05-29 05:36:04 +0000

    Update script for difftgen

--- experiments/scripts/difftgen.py
+++ experiments/scripts/difftgen.py
@@ -7,9 +7,9 @@ def run(tool:str):
     difftgen_dir = os.path.abspath(f"{root_dir}/DiffTGen")
     result=subprocess.run(['python3','script/extract-candidates.py',tool.lower(), root_dir, f'patches/{tool.lower()}'],stdout=subprocess.PIPE,
                           stderr=subprocess.STDOUT, cwd=difftgen_dir)
-    result=subprocess.run(['python3','../DiffTGen/script/driver.py', tool.lower(), f'patches/{tool.lower()}'],stdout=subprocess.PIPE,
+    result=subprocess.run(['python3','script/driver.py', root_dir, tool.lower(), f'patches/{tool.lower()}'],stdout=subprocess.PIPE,
                           stderr=subprocess.STDOUT, cwd=difftgen_dir)
-    result=subprocess.run(['python3','script/check-results.py', tool.lower(),f'patches/{tool.lower()}'],stdout=subprocess.PIPE,
+    result=subprocess.run(['python3','script/check-results.py', root_dir, tool.lower(),f'patches/{tool.lower()}'],stdout=subprocess.PIPE,
                           stderr=subprocess.STDOUT, cwd=difftgen_dir)
     
     shutil.copytree(f'{difftgen_dir}/out/{tool.lower()}/{tool.lower()}.csv',f'{tool.lower()}/difftgen.csv',dirs_exist_ok=True)
