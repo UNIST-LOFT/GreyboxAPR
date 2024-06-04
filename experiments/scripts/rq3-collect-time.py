57478528561646812024b057bddc8d1c7f569321
FreddyYJ <kyj1411@unist.ac.kr>
2024-06-04 03:10:27 +0000

    Add time for RQ3

--- experiments/scripts/rq3-collect-time.py
+++ experiments/scripts/rq3-collect-time.py
@@ -1,5 +1,6 @@
 from getopt import getopt
 import os
+from statistics import mean
 from sys import argv
 from typing import Dict, List
 import json
@@ -13,7 +14,7 @@ import d4j
 
 MAX_EXP=10
 WITH_MOCKITO=False
-MAX_ITERATION=3000
+MAX_TIME=300
 
 def plot_patches_ci_java(mode='tbar'):
     orig_result:List[int]=[]
@@ -38,17 +39,33 @@ def plot_patches_ci_java(mode='tbar'):
             root=json.load(result_file)
             result_file.close()
 
-            prev_time=0.
+            # Read cache to get baseline time
+            try:
+                cache_file=open(f'{mode}/result/cache/{result}-cache.json','r')
+            except:
+                continue
+            cache=json.load(cache_file)
+            cache_file.close()
+
+
+            total_time=0.
             for res in root:
                 is_hq=res['result']
                 is_plausible=res['pass_result']
                 iteration=res['iteration']
                 time=res['time']
                 loc=res['config'][0]['location']
+                if loc in cache:
+                    total_time+=cache[loc]['fail_time']+cache[loc]['pass_time']
+                else:
+                    if res['time']-total_time>0:
+                        total_time+=(res['time']-total_time)
 
                 if is_plausible:
-                    # casino_result[i].append(round((time)/60))
-                    casino_result[i].append(iteration)
+                    if MAX_TIME<round((total_time)/60):
+                        MAX_TIME=round((total_time)/60)
+                    casino_result[i].append(round((time)/60))
+                    # casino_result[i].append(iteration)
 
                 # if time>3600:
                 #     break
@@ -71,7 +88,22 @@ def plot_patches_ci_java(mode='tbar'):
             root=json.load(result_file)
             result_file.close()
 
-            prev_time=0.
+            # Read cache to get baseline time
+            try:
+                cache_file=open(f'{mode}/result/cache/{result}-cache.json','r')
+            except:
+                continue
+            cache=json.load(cache_file)
+            cache_file.close()
+
+            file_instrument_time:Dict[str,float]=dict()
+            with open(f'scripts/file-instrument-time/{result}.txt','r') as f:
+                for line in f:
+                    file,time=line.strip().split(',')
+                    file_instrument_time[file]=float(time)
+
+            total_time=0.
+            fail_time_list=[]
             for res in root:
                 is_hq=res['result']
                 is_plausible=res['pass_result']
@@ -79,9 +111,20 @@ def plot_patches_ci_java(mode='tbar'):
                 time=res['time']
                 loc=res['config'][0]['location']
 
+                if 'fail_time' in cache[loc]:
+                    total_time+=cache[loc]['fail_time']+cache[loc]['pass_time']
+                    fail_time_list.append(cache[loc]['fail_time'])
+                else:
+                    total_time+=mean(fail_time_list)+cache[loc]['pass_time']
+                if is_hq:
+                    # Instrumentation time
+                    total_time+=file_instrument_time[loc.split('/')[-1]]
+
                 if is_plausible:
-                    # wo_vertical[i].append(round((time)/60))
-                    wo_vertical[i].append(iteration)
+                    if MAX_TIME<round((total_time)/60):
+                        MAX_TIME=round((total_time)/60)
+                    wo_vertical[i].append(round((time)/60))
+                    # wo_vertical[i].append(iteration)
 
                 # if time>3600:
                 #     break
@@ -104,7 +147,22 @@ def plot_patches_ci_java(mode='tbar'):
             root=json.load(result_file)
             result_file.close()
 
-            prev_time=0.
+            # Read cache to get baseline time
+            try:
+                cache_file=open(f'{mode}/result/cache/{result}-cache.json','r')
+            except:
+                continue
+            cache=json.load(cache_file)
+            cache_file.close()
+
+            file_instrument_time:Dict[str,float]=dict()
+            with open(f'scripts/file-instrument-time/{result}.txt','r') as f:
+                for line in f:
+                    file,time=line.strip().split(',')
+                    file_instrument_time[file]=float(time)
+
+            total_time=0.
+            fail_time_list=[]
             for res in root:
                 is_hq=res['result']
                 is_plausible=res['pass_result']
@@ -112,9 +170,20 @@ def plot_patches_ci_java(mode='tbar'):
                 time=res['time']
                 loc=res['config'][0]['location']
 
+                if 'fail_time' in cache[loc]:
+                    total_time+=cache[loc]['fail_time']+cache[loc]['pass_time']
+                    fail_time_list.append(cache[loc]['fail_time'])
+                else:
+                    total_time+=mean(fail_time_list)+cache[loc]['pass_time']
+                if is_hq:
+                    # Instrumentation time
+                    total_time+=file_instrument_time[loc.split('/')[-1]]
+
                 if is_plausible:
-                    # greybox_result[i].append(round((time)/60))
-                    greybox_result[i].append(iteration)
+                    if MAX_TIME<round((total_time)/60):
+                        MAX_TIME=round((total_time)/60)
+                    greybox_result[i].append(round((time)/60))
+                    # greybox_result[i].append(iteration)
 
                 # if time>3600:
                 #     break
@@ -136,24 +205,40 @@ def plot_patches_ci_java(mode='tbar'):
         root=json.load(result_file)
         result_file.close()
 
-        prev_time=0.
+        # Read cache to get baseline time
+        try:
+            cache_file=open(f'{mode}/result/cache/{result}-cache.json','r')
+        except:
+            continue
+        cache=json.load(cache_file)
+        cache_file.close()
+
+        total_time=0.
         for res in root:
             is_hq=res['result']
             is_plausible=res['pass_result']
             iteration=res['iteration']
             time=res['time']
+            if loc in cache:
+                total_time+=cache[loc]['fail_time']+cache[loc]['pass_time']
+            else:
+                if res['time']-total_time>0:
+                    total_time+=(res['time']-total_time)
+
             loc=res['config'][0]['location']
 
             if is_plausible:
-                # orig_result.append(round((time)/60))
-                orig_result.append(iteration)
+                if MAX_TIME<round((total_time)/60):
+                    MAX_TIME=round((total_time)/60)
+                orig_result.append(round((time)/60))
+                # orig_result.append(iteration)
 
             # if time>3600:
             #     break
 
     print(len(orig_result))
 
-    with open(f'rq3-{mode}.json','w') as f:
+    with open(f'rq3-{mode}-time.json','w') as f:
         json.dump({
             'orig':orig_result,
             'wo_vertical':wo_vertical,
@@ -168,12 +253,12 @@ def plot_patches_ci_java(mode='tbar'):
     # Original
     results=sorted(orig_result)
     other_list=[0]
-    for i in range(0,MAX_ITERATION):
+    for i in range(0,MAX_TIME+1):
         if i in results:
             other_list.append(other_list[-1]+results.count(i))
         else:
             other_list.append(other_list[-1])
-    plt.plot(list(range(0,MAX_ITERATION+1)),other_list,'-.b',label='Orig')
+    plt.plot(list(range(0,MAX_TIME+2)),other_list,'-.b',label='Orig')
 
     # Casino
     guided_list:List[List[int]]=[]
@@ -182,7 +267,7 @@ def plot_patches_ci_java(mode='tbar'):
     for j in range(MAX_EXP):
         cur_result=sorted(casino_result[j])
         guided_list.append([0])
-        for i in range(0,MAX_ITERATION+1):
+        for i in range(0,MAX_TIME+1):
             if i in cur_result:
                 guided_list[-1].append(guided_list[-1][-1]+cur_result.count(i)/MAX_EXP)
                 guided_x.append(i)
@@ -197,8 +282,8 @@ def plot_patches_ci_java(mode='tbar'):
                     guided_y.append(0)
                 else:
                     guided_y.append(guided_y[-1])
-    guided_df=pd.DataFrame({'Iteration':guided_x,'# of valid patches':guided_y})
-    seaborn.lineplot(data=guided_df,x='Iteration',y='# of valid patches',color='g',label='Casino')
+    guided_df=pd.DataFrame({'Time (min)':guided_x,'# of valid patches':guided_y})
+    seaborn.lineplot(data=guided_df,x='Time (min)',y='# of valid patches',color='g',label='Casino')
 
     # w/o vertical
     guided_list:List[List[int]]=[]
@@ -207,7 +292,7 @@ def plot_patches_ci_java(mode='tbar'):
     for j in range(MAX_EXP):
         cur_result=sorted(wo_vertical[j])
         guided_list.append([0])
-        for i in range(0,MAX_ITERATION+1):
+        for i in range(0,MAX_TIME+1):
             if i in cur_result:
                 guided_list[-1].append(guided_list[-1][-1]+cur_result.count(i)/MAX_EXP)
                 guided_x.append(i)
@@ -222,8 +307,8 @@ def plot_patches_ci_java(mode='tbar'):
                     guided_y.append(0)
                 else:
                     guided_y.append(guided_y[-1])
-    guided_df=pd.DataFrame({'Iteration':guided_x,'# of valid patches':guided_y})
-    seaborn.lineplot(data=guided_df,x='Iteration',y='# of valid patches',color='y',label='w/o 1st vert.')
+    guided_df=pd.DataFrame({'Time (min)':guided_x,'# of valid patches':guided_y})
+    seaborn.lineplot(data=guided_df,x='Time (min)',y='# of valid patches',color='y',label='w/o 1st vert.')
 
     # greybox
     other_list:List[List[int]]=[]
@@ -232,7 +317,7 @@ def plot_patches_ci_java(mode='tbar'):
     for j in range(MAX_EXP):
         cur_result=sorted(greybox_result[j])
         other_list.append([0])
-        for i in range(0,MAX_ITERATION+1):
+        for i in range(0,MAX_TIME+1):
             if i in cur_result:
                 other_list[-1].append(other_list[-1][-1]+cur_result.count(i)/MAX_EXP)
                 other_x.append(i)
@@ -247,20 +332,20 @@ def plot_patches_ci_java(mode='tbar'):
                     other_y.append(0)
                 else:
                     other_y.append(other_y[-1])
-    other_df=pd.DataFrame({'Iteration':other_x,'# of valid patches':other_y})
-    seaborn.lineplot(data=other_df,x='Iteration',y='# of valid patches',color='r',label='Gresino',linestyle='dashed')
+    other_df=pd.DataFrame({'Time (min)':other_x,'# of valid patches':other_y})
+    seaborn.lineplot(data=other_df,x='Time (min)',y='# of valid patches',color='r',label='Gresino',linestyle='dashed')
 
     plt.legend(fontsize=12)
-    plt.xlabel('Iteration',fontsize=15)
+    plt.xlabel('Time (min)',fontsize=15)
     plt.ylabel('# of Valid Patches',fontsize=15)
     plt.xticks(fontsize=15)
     plt.yticks(fontsize=15)
     if WITH_MOCKITO:
-        plt.savefig(f'rq3-{mode}-w-mockito.pdf',bbox_inches='tight')
-        plt.savefig(f'rq3-{mode}-w-mockito.jpg',bbox_inches='tight')
+        plt.savefig(f'rq3-time-{mode}-w-mockito.pdf',bbox_inches='tight')
+        plt.savefig(f'rq3-time-{mode}-w-mockito.jpg',bbox_inches='tight')
     else:
-        plt.savefig(f'rq3-{mode}.pdf',bbox_inches='tight')
-        plt.savefig(f'rq3-{mode}.jpg',bbox_inches='tight')
+        plt.savefig(f'rq3-time-{mode}.pdf',bbox_inches='tight')
+        plt.savefig(f'rq3-time-{mode}.jpg',bbox_inches='tight')
 
 o,a=getopt(argv[1:],'',['with-mockito'])
 for opt,arg in o:
--- experiments/scripts/rq3-time.py
+++ experiments/scripts/rq3-time.py
@@ -12,7 +12,7 @@ import d4j
 
 MAX_EXP=10
 WITH_MOCKITO=False
-MAX_ITERATION=3000
+MAX_TIME=300
 
 orig_result:List[int]=[]
 wo_vertical:List[List[int]]=[[] for _ in range(MAX_EXP)]
@@ -52,12 +52,12 @@ fig=plt.figure(figsize=(5,3))
 # Original
 results=sorted(orig_result)
 other_list=[0]
-for i in range(0,MAX_ITERATION):
+for i in range(0,MAX_TIME+1):
     if i in results:
         other_list.append(other_list[-1]+results.count(i))
     else:
         other_list.append(other_list[-1])
-plt.plot(list(range(0,MAX_ITERATION+1)),other_list,'-.b',label='Orig')
+plt.plot(list(range(0,MAX_TIME+2)),other_list,'-.b',label='Orig')
 
 # Casino
 guided_list:List[List[int]]=[]
@@ -66,7 +66,7 @@ guided_y=[]
 for j in range(MAX_EXP):
     cur_result=sorted(casino_result[j])
     guided_list.append([0])
-    for i in range(0,MAX_ITERATION+1):
+    for i in range(0,MAX_TIME+1):
         if i in cur_result:
             guided_list[-1].append(guided_list[-1][-1]+cur_result.count(i)/MAX_EXP)
             guided_x.append(i)
@@ -81,8 +81,8 @@ for j in range(MAX_EXP):
                 guided_y.append(0)
             else:
                 guided_y.append(guided_y[-1])
-guided_df=pd.DataFrame({'Iteration':guided_x,'# of valid patches':guided_y})
-seaborn.lineplot(data=guided_df,x='Iteration',y='# of valid patches',color='g',label='Casino')
+guided_df=pd.DataFrame({'Time (min)':guided_x,'# of valid patches':guided_y})
+seaborn.lineplot(data=guided_df,x='Time (min)',y='# of valid patches',color='g',label='Casino')
 
 # w/o vertical
 guided_list:List[List[int]]=[]
@@ -91,7 +91,7 @@ guided_y=[]
 for j in range(MAX_EXP):
     cur_result=sorted(wo_vertical[j])
     guided_list.append([0])
-    for i in range(0,MAX_ITERATION+1):
+    for i in range(0,MAX_TIME+1):
         if i in cur_result:
             guided_list[-1].append(guided_list[-1][-1]+cur_result.count(i)/MAX_EXP)
             guided_x.append(i)
@@ -106,8 +106,8 @@ for j in range(MAX_EXP):
                 guided_y.append(0)
             else:
                 guided_y.append(guided_y[-1])
-guided_df=pd.DataFrame({'Iteration':guided_x,'# of valid patches':guided_y})
-seaborn.lineplot(data=guided_df,x='Iteration',y='# of valid patches',color='y',label='w/o 1st vert.')
+guided_df=pd.DataFrame({'Time (min)':guided_x,'# of valid patches':guided_y})
+seaborn.lineplot(data=guided_df,x='Time (min)',y='# of valid patches',color='y',label='w/o 1st vert.')
 
 # greybox
 other_list:List[List[int]]=[]
@@ -116,7 +116,7 @@ other_y=[]
 for j in range(MAX_EXP):
     cur_result=sorted(greybox_result[j])
     other_list.append([0])
-    for i in range(0,MAX_ITERATION+1):
+    for i in range(0,MAX_TIME+1):
         if i in cur_result:
             other_list[-1].append(other_list[-1][-1]+cur_result.count(i)/MAX_EXP)
             other_x.append(i)
@@ -131,18 +131,18 @@ for j in range(MAX_EXP):
                 other_y.append(0)
             else:
                 other_y.append(other_y[-1])
-other_df=pd.DataFrame({'Iteration':other_x,'# of valid patches':other_y})
-seaborn.lineplot(data=other_df,x='Iteration',y='# of valid patches',color='r',label='Gresino',linestyle='dashed')
+other_df=pd.DataFrame({'Time (min)':other_x,'# of valid patches':other_y})
+seaborn.lineplot(data=other_df,x='Time (min)',y='# of valid patches',color='r',label='Gresino',linestyle='dashed')
 
 plt.legend(fontsize=12)
-plt.xlabel('Iteration',fontsize=15)
+plt.xlabel('Time (min)',fontsize=15)
 plt.ylabel('# of Valid Patches',fontsize=15)
 plt.xticks(fontsize=15)
 plt.locator_params(axis='x',nbins=6)
 plt.yticks(fontsize=15)
 if WITH_MOCKITO:
-    plt.savefig(f'rq3-w-mockito.pdf',bbox_inches='tight')
-    plt.savefig(f'rq3-w-mockito.jpg',bbox_inches='tight')
+    plt.savefig(f'rq3-time-w-mockito.pdf',bbox_inches='tight')
+    plt.savefig(f'rq3-time-w-mockito.jpg',bbox_inches='tight')
 else:
-    plt.savefig(f'rq3.pdf',bbox_inches='tight')
-    plt.savefig(f'rq3.jpg',bbox_inches='tight')
+    plt.savefig(f'rq3-time.pdf',bbox_inches='tight')
+    plt.savefig(f'rq3-time.jpg',bbox_inches='tight')
