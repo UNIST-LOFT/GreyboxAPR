d0d6ce30c2bff42afe70ba84e6fec6050d2bd888
FreddyYJ <kyj1411@unist.ac.kr>
2024-06-07 04:07:19 +0000

    Add more info for temp script

--- experiments/scripts/.temp-parse-log.py
+++ experiments/scripts/.temp-parse-log.py
@@ -1,7 +1,7 @@
 import json
-from os import path
+from os import path,listdir
 from sys import argv
-from typing import Dict, Set
+from typing import Dict, List, Set
 import d4j
 import random
 
@@ -9,6 +9,24 @@ MAX_EXP=10
 WITH_MOCKITO=False
 MAX_ITERATION=3000
 random.seed(0)
+
+def parse_branch(file_path:str):
+    result:Dict[int,int]=dict()
+    with open(file_path,'r') as file:
+        for line in file:
+            if ':' in line:
+                id,count=line.split(':')
+                result[int(id)]=int(count)
+    return result
+
+def branch_diff(orig:Dict[int,int],patch:Dict[int,int]):
+    result:List[int]=[]
+    for key in orig:
+        if key not in patch:
+            result.append(key)
+        elif orig[key]!=patch[key]:
+            result.append(key)
+    return result
 
 def parse(mode:str):
     casino_result:Dict[str,int]={
@@ -19,6 +37,12 @@ def parse(mode:str):
         '1st':0,
         '2nd':0,
         'hor':0
+    }
+
+    casino_update_result=0
+    greybox_update_result={
+        'blackbox':0,
+        'greybox':0
     }
 
     for i in range(MAX_EXP):
@@ -36,11 +60,28 @@ def parse(mode:str):
             root=json.load(result_file)
             result_file.close()
 
+            # Parse original branch coverage
+            orig_coverage:Dict[str,Dict[int,int]]=dict()
+            for file in listdir(f'{mode}/result/branch/{result}'):
+                if file.startswith('original_'):
+                    test=file.split('_')[1].split('.')[0]
+                    orig_coverage[test]=parse_branch(f'{mode}/result/branch/{result}/{file}')
+
             intr_patches:Set[str]=set()
             valid_patches:Set[str]=set()
             for res in root:
                 if res['result']:
                     intr_patches.add(res['config'][0]['location'])
+                    greybox_update_result['blackbox']+=4
+
+                    for file in listdir(f'{mode}/result/branch/{result}'):
+                        if file.startswith(res['config'][0]['location'].replace('/','#')):
+                            test=file.split('_')[1].split('.')[0]
+                            patch_coverage=parse_branch(f'{mode}/result/branch/{result}/{file}')
+                            diff=branch_diff(orig_coverage[test],patch_coverage)
+                            greybox_update_result['blackbox']+=len(diff)*4
+                            continue
+
                 if res['pass_result']:
                     valid_patches.add(res['config'][0]['location'])
 
@@ -61,8 +102,22 @@ def parse(mode:str):
                     greybox_result['hor']+=1
                     casino_result['hor']+=1
 
+            result_log.close()
+
+            try:
+                r_file=open(f'{mode}/result/{result}-casino-{i}/simapr-result.json','r')
+            except:
+                continue
+            root=json.load(r_file)
+            r_file.close()
+            for res in root:
+                if res['result']:
+                    casino_update_result+=1
+
     print(mode)
     print(f'Greybox: {greybox_result}')
     print(f'Casino: {casino_result}')
+    print(f'Greybox: {greybox_update_result}')
+    print(f'Casino: {casino_update_result}')
 
 parse(argv[1])
