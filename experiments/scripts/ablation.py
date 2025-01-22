80954edad3a7f4e40ba94d3ab90908dfddc78707
chan-hwi <chanhwi@unist.ac.kr>
2025-01-22 15:01:54 +0000

    Refactor rq3-collect.py

---
+++ experiments/scripts/ablation.py
@@ -0,0 +1,80 @@
+import os
+from typing import List
+import json
+
+import numpy as np
+import pandas as pd
+import seaborn
+
+import d4j
+
+deps = ['greyboxfd', 'greybox-fieldonly', 'wo-vertical-both']
+
+class AblationResult:
+    def __init__(self, mode: str, name: str, dirname: str, color: str):
+        self.mode = mode
+        self.name = name
+        self.dirname = dirname
+        self.color = color
+        self.result_list: List[List[int]] = [[] for _ in range(MAX_EXP)]
+        
+    def check_deps(self, result):
+        for dep in deps:
+            if not os.path.exists(f'{self.mode}/result/{result}-{dep}-{MAX_EXP-1}/simapr-finished.txt'):
+                return False
+        return True
+        
+    def save_result(self):
+        for i in range(MAX_EXP):
+            for result in d4j.D4J_1_2_LIST:
+                if self.check_deps(result):
+                    # Skip if experiment not end
+                    continue
+                if not WITH_MOCKITO and 'Mockito' in result:
+                    continue
+
+                try:
+                    result_file=open(f'{self.mode}/result/{self.result}-{self.dirname}-{i}/simapr-result.json','r')
+                except:
+                    continue
+                root=json.load(result_file)
+                result_file.close()
+
+                prev_time=0.
+                for res in root:
+                    is_hq=res['result']
+                    is_plausible=res['pass_result']
+                    iteration=res['iteration']
+                    time=res['time']
+                    loc=res['config'][0]['location']
+
+                    if is_plausible:
+                        self.result_list[i].append(iteration)
+
+        print(self.name)
+        print(np.mean([len(l) for l in self.result_list]))
+    
+    def draw_plot(self):
+        guided_list:List[List[int]]=[]
+        guided_x=[]
+        guided_y=[]
+        for j in range(MAX_EXP):
+            cur_result=sorted(self.result_list[j])
+            guided_list.append([0])
+            for i in range(0,MAX_ITERATION+1):
+                if i in cur_result:
+                    guided_list[-1].append(guided_list[-1][-1]+cur_result.count(i)/MAX_EXP)
+                    guided_x.append(i)
+                    if i==0:
+                        guided_y.append(0)
+                    else:
+                        guided_y.append(guided_y[-1]+cur_result.count(i))
+                else:
+                    guided_list[-1].append(guided_list[-1][-1])
+                    guided_x.append(i)
+                    if i==0:
+                        guided_y.append(0)
+                    else:
+                        guided_y.append(guided_y[-1])
+        guided_df=pd.DataFrame({'Iteration':guided_x,'# of valid patches':guided_y})
+        seaborn.lineplot(data=guided_df,x='Iteration',y='# of valid patches',color=self.color,label=self.name)
--- experiments/scripts/rq3-collect.py
+++ experiments/scripts/rq3-collect.py
@@ -1,120 +1,18 @@
 from getopt import getopt
-import os
 from sys import argv
-from typing import Dict, List
+from typing import List
 import json
+import os
 
 from matplotlib import pyplot as plt
-import numpy as np
-import pandas as pd
-import seaborn
 
 import d4j
+
+from ablation import AblationResult
 
 MAX_EXP=10
 WITH_MOCKITO=False
 MAX_ITERATION=3000
-
-deps = ['greybox', 'wo-vertical']
-
-def save_result(mode: str, dirname: str, result_list: List[List[int]]):
-    for i in range(MAX_EXP):
-        for result in d4j.D4J_1_2_LIST:
-            if check_deps(mode, result):
-                # Skip if experiment not end
-                continue
-            if not WITH_MOCKITO and 'Mockito' in result:
-                continue
-
-            try:
-                result_file=open(f'{mode}/result/{result}-{dirname}-{i}/simapr-result.json','r')
-            except:
-                continue
-            root=json.load(result_file)
-            result_file.close()
-
-            prev_time=0.
-            for res in root:
-                is_hq=res['result']
-                is_plausible=res['pass_result']
-                iteration=res['iteration']
-                time=res['time']
-                loc=res['config'][0]['location']
-
-                if is_plausible:
-                    result_list[i].append(iteration)
-
-    print(name)
-    print(np.mean([len(l) for l in result_list]))
-
-class AblationResult:
-    def __init__(self, mode: str, name: str, dirname: str, color: str):
-        self.mode = mode
-        self.name = name
-        self.dirname = dirname
-        self.color = color
-        self.result_list: List[List[int]] = [[] for _ in range(MAX_EXP)]
-        
-    def check_deps(self, result):
-        for dep in deps:
-            if not os.path.exists(f'{self.mode}/result/{result}-{dep}-{MAX_EXP-1}/simapr-finished.txt'):
-                return False
-        return True
-        
-    def save_result(self):
-        for i in range(MAX_EXP):
-            for result in d4j.D4J_1_2_LIST:
-                if self.check_deps(result):
-                    # Skip if experiment not end
-                    continue
-                if not WITH_MOCKITO and 'Mockito' in result:
-                    continue
-
-                try:
-                    result_file=open(f'{self.mode}/result/{self.result}-{self.dirname}-{i}/simapr-result.json','r')
-                except:
-                    continue
-                root=json.load(result_file)
-                result_file.close()
-
-                prev_time=0.
-                for res in root:
-                    is_hq=res['result']
-                    is_plausible=res['pass_result']
-                    iteration=res['iteration']
-                    time=res['time']
-                    loc=res['config'][0]['location']
-
-                    if is_plausible:
-                        self.result_list[i].append(iteration)
-
-        print(self.name)
-        print(np.mean([len(l) for l in self.result_list]))
-    
-    def draw_plot(self):
-        guided_list:List[List[int]]=[]
-        guided_x=[]
-        guided_y=[]
-        for j in range(MAX_EXP):
-            cur_result=sorted(self.result_list[j])
-            guided_list.append([0])
-            for i in range(0,MAX_ITERATION+1):
-                if i in cur_result:
-                    guided_list[-1].append(guided_list[-1][-1]+cur_result.count(i)/MAX_EXP)
-                    guided_x.append(i)
-                    if i==0:
-                        guided_y.append(0)
-                    else:
-                        guided_y.append(guided_y[-1]+cur_result.count(i))
-                else:
-                    guided_list[-1].append(guided_list[-1][-1])
-                    guided_x.append(i)
-                    if i==0:
-                        guided_y.append(0)
-                    else:
-                        guided_y.append(guided_y[-1])
-        guided_df=pd.DataFrame({'Iteration':guided_x,'# of valid patches':guided_y})
-        seaborn.lineplot(data=guided_df,x='Iteration',y='# of valid patches',color=self.color,label=self.name)
 
 def plot_patches_ci_java(mode='tbar'):
     orig_result:List[int]=[]
